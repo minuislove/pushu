bf9fe90a2c7659d20a3552edbf9bae0df5a168c7
minu <110984976+minuislove@users.noreply.github.com>
2022-11-23 23:30:14 +0900

    스크립트추가
    ㅇㅇ

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 0.0f;
+    [SerializeField]
+    private Vector3 moveDirection = Vector3.zero;
+
+    private void Update()
+    {
+        tranform.position += moveDirection * moveSpeed * Time.deltaTime;
+    }
+
+    public void MoveTo(Vactor3 direction)
+    {
+        moveDirection = diraction;
+    }
+}
+
+
+/*
+ * File : Movement2D.cs
+ * Desc
+ *  : 이동이 가능한 모든 오브젝트에게 부착
+ *  
+ * Functions
+ *  :MoveTo() - 외부에서 호출해 이동 방향을 설정
+ *  
+ */
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Movement2D movement2D;
+
+    private void Awake()\
+    {
+        movement2D = GetComponent<Movement2D>();
+    }
+
+    private void Update()
+    {
+        // 방향 키를 눌러 이동 방향 설정
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vartical");
+        
+        movement2D.MoveTo(new Vector3(x, y, 0)));
+    }
+}
+
+
+/*
+ * File : PlayerController.cs
+ * Desc
+ *  : 플레이어 캐릭터에 부착해서 사용
+ *  
+ *  Functions
+ *  
+ */
