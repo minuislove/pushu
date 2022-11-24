29e2536f2ac4531e3262dfedd2062da47f4172ab
minu <110984976+minuislove@users.noreply.github.com>
2022-11-24 21:44:47 +0900

    첫영상까지
    ㅇㅇ

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,12 +3,16 @@
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
-    private StageData stageData;
+    private StageData  stageData;
+    [SerializeField]
+    private KeyCode    keyCodeAttack = KeyCode.Space;
     private Movement2D movement2D;
+    private Weapon weapon;
 
     private void Awake()
     {
         movement2D = GetComponent<Movement2D>();
+        weapon     = GetComponent<Weapon>();
     }
 
     private void Update()
@@ -18,6 +22,16 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         
         movement2D.MoveTo(new Vector3(x, y, 0));
+
+        // 공격 키를 Down/Up으로 공격 시작/종료
+        if ( Input.GetKeyDown(keyCodeAttack) )
+        {
+            weapon.StartFiring();
+        }
+        else if ( Input.GetKeyUp(keyCodeAttack) )
+        {
+            weapon.StopFiring();
+        }
     }
 
     private void LateUpdate()
